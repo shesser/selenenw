31eadb92c95757c5db8bc048862e2c2f9b5353c3
manimalik <manimalik20@gmail.com>
2015-09-20 19:03:23 +0500

    Additions for SeleneNW listings

--- themes/selene/get-yacht-listing.php
+++ themes/selene/get-yacht-listing.php
@@ -1,8 +1,8 @@
 <?php /* Template Name: Get Yacht Listing */
 ini_set('max_execution_time', 0);
-save_yacht_listing('http://www.yachtworld.com/privatelabel/listing/cache/pl_search_results.jsp?slim=pp289556&cit=true&sm=3&is=false&man=Selene&fromLength=&toLength=&luom=126&fromYear=&toYear=&fromPrice=&toPrice=&currencyid=100&hmid=&ftid=&enid=&city=&spid=&rid=&cint=&msint=&ps=100&ErrorMessage=Please%20check%20one%20or%20more%20boats.', false);
+save_yacht_listing('http://www.yachtworld.com/privatelabel/listing/cache/pl_search_results.jsp?slim=pp289556&cit=true&sm=3&is=false&man=Selene&fromLength=&toLength=&luom=126&fromYear=&toYear=&fromPrice=&toPrice=&currencyid=100&hmid=&ftid=&enid=&city=&spid=&rid=&cint=&msint=&ps=100&ErrorMessage=Please%20check%20one%20or%20more%20boats.', 0);
 
-save_yacht_listing('http://www.yachtworld.com/core/listing/cache/pl_search_results.jsp?ywo=seleneyachtsnorthwest&ps=50&type=&new=&luom=126&hosturl=seleneyachtsnorthwest&page=broker&slim=pp289556&lineonly', true);
+save_yacht_listing('http://www.yachtworld.com/core/listing/cache/pl_search_results.jsp?ywo=seleneyachtsnorthwest&ps=50&type=&new=&luom=126&hosturl=seleneyachtsnorthwest&page=broker&slim=pp289556&lineonly', 1);
 
 fetch_yachtworld_detail();
 fetch_yachtworld_images();
