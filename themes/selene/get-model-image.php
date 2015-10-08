58ba1ecfda92a427dec6eb5a1ba5f37b80b2e028
manimalik <manimalik20@gmail.com>
2015-10-08 23:35:53 +0500

    updates to get images for yachts

--- themes/selene/get-model-image.php
+++ themes/selene/get-model-image.php
@@ -3,10 +3,19 @@
  Template Name: Temp Get Image
  */
 
-if(isset($_POST['url'])) {
- $file = $_POST['url'];
- echo get_yacht_image ( $file ).'<br/>';
+if(isset($_POST['urls'])) {
+    $urls = json_decode( $_POST['urls'] );
+    $images = array();
+    foreach ( $urls as $url ) {
+        $result = get_yacht_image ( $url );
+        $images[] = array(
+            'original' => str_replace( '640x465_', '', $result),
+            'resized' => $result,
+        );
+    }
+
+    var_dump( json_encode( $images ) );
 }
 
 
-echo '<form action="' . get_permalink() . '" method="post"><input type="text" name="url"><input type="submit" value="Get Images" name="get-images"></form>';
+echo '<form action="' . get_permalink() . '" method="post"><textarea name="urls"></textarea><input type="submit" value="Get Images" name="get-images"></form>';
